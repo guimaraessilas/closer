3460b12f91cdfc993a463ae61b094da5cfe9e8bb
silas <guimaraessilas>
2018-11-15 20:53:35 -0200

    tela de login finalizada

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Spinner } from 'native-base';
 import LoginScreen from './src/screens/login/LoginScreen';
 
 export default class App extends Component {
@@ -10,18 +9,21 @@ export default class App extends Component {
       loading:true,
     }
   }
+  
   async componentWillMount() {
     await Expo.Font.loadAsync({
-      'Roboto': require('native-base/Fonts/Roboto.ttf'),
-      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-    })
-    .then(this.setState({loading:false}));
+        Roboto: require("native-base/Fonts/Roboto.ttf"),
+        Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+        Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+    });
+    this.setState({ loading: false });
+
   }
   
   render() {
     
     if(this.state.loading){
-      return <Spinner/>
+      return <Expo.AppLoading/>
     }else{
       return <LoginScreen/>
     }
--- src/screens/login/LoginScreen.js
+++ src/screens/login/LoginScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Container, Text, Form, Content, Item, Input, Label, Header, Title } from 'native-base';
+import { Text, Button, Icon, View, Form, Label, Item, Input } from 'native-base';
+import { StyleSheet, ImageBackground } from 'react-native';
 
 export default class LoginScreen extends Component {
     constructor(props) {
@@ -11,28 +12,68 @@ export default class LoginScreen extends Component {
 
     async componentWillMount() {
         await Expo.Font.loadAsync({
-            'Roboto': require('native-base/Fonts/Roboto.ttf'),
-            'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-        })
-            .then(this.setState({ loading: false }));
+            Roboto: require("native-base/Fonts/Roboto.ttf"),
+            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+            Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+        });
+        this.setState({ loading: false });
+
     }
 
     render() {
 
         if (this.state.loading) {
-            return <Spinner />
+            return <Expo.AppLoading />
         } else {
-            return <Container>
-                        <Header><Title>Entre ou Cadastre-se!</Title></Header>
-                        <Content>
-                            <Form>
-                                <Item floatingLabel>
-                                    <Label>Email:</Label>
-                                    <Input/>
-                                </Item>
-                            </Form>
-                        </Content>
-                    </Container>
+            return (
+                <ImageBackground source={require('../../../assets/login-background.jpg')} style={styles.backgroundImage} >
+
+                    <Button transparent style={{ marginTop: 22, marginLeft: 5 }}>
+                        <Icon name="add" style={{ color: '#fff', fontWeight: 'bold' }} />
+                        <Text style={styles.textStyle}>Criar uma conta</Text>
+                    </Button>
+
+                    <View style={styles.container}>
+                        <Form>
+                            <Item floatingLabel style={{ marginRight: 15 }}>
+                                <Label style={styles.textStyle}>Username or Email</Label>
+                                <Input style={styles.textStyle}/>
+                                <Icon name="mail" style={{color: '#fff'}}/>
+                            </Item>
+                            <Item floatingLabel style={{ marginRight: 15 }}>
+                                <Label style={styles.textStyle}>Password</Label>
+                                <Input style={styles.textStyle}/>
+                                <Icon name="lock" style={{color: '#fff'}}/>
+                            </Item>
+                        </Form>
+                        <Button full style={{ marginTop: 10, marginLeft: 15, marginRight: 15, backgroundColor: '#066039' }} onPress={() => { console.log('entrar') }}>
+                            <Text> Entrar </Text>
+                        </Button>
+                    </View>
+                </ImageBackground >
+            );
         }
     }
 }
+
+const styles = StyleSheet.create({
+    backgroundImage: {
+        backgroundColor: '#00000022',
+        flex: 1,
+    },
+    textStyle: {
+        color: '#fff',
+        fontSize: 16,
+    },
+    container: {
+        justifyContent: 'center',
+        marginTop: 30,
+        paddingTop: 100,
+        paddingLeft: 5,
+        paddingRight: 5,
+        paddingBottom: 100,
+        backgroundColor: '#00000055',
+
+    },
+
+});
